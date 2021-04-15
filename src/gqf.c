33b45fb946c7853475ec9a1927ff9a339037dcc6
Ericwimsatt <ericwimsatt@gmail.com>
2021-04-15 15:38:18 -0700

    fd

--- src/gqf.c
+++ src/gqf.c
@@ -1930,7 +1930,7 @@ static inline int find_thread_start(QF* qf, uint64_t* keys, int tid, int num_thr
 	uint64_t max_quotient = 1ULL << qbits;
 	printf("max %lx", max_quotient);
 	uint64_t thread_min_quotient = ceil(max_quotient / num_threads) * tid;
-	printf("tid %d, overall max quotient %lx, thread min quotient %lx", tid, max_quotient, thread_min_quotient);
+	printf("tid %d, overall max quotient %lx, thread min quotient %lx \n", tid, max_quotient, thread_min_quotient);
 	for (int i = 0; i < nvals; i++) {
 
 		uint64_t quotient = keys[i] >> qf->metadata->bits_per_slot;
@@ -1943,7 +1943,11 @@ static inline int find_thread_start(QF* qf, uint64_t* keys, int tid, int num_thr
 
 void qf_insert_gpu(QF* qf, uint64_t* keys, uint64_t value, uint64_t count, uint64_t nvals, uint64_t nslots, uint64_t qbits, uint8_t
 	flags) {
+	find_thread_start(qf, keys, 1, 6, nvals, qbits);
 	find_thread_start(qf, keys, 2, 6, nvals, qbits);
+	find_thread_start(qf, keys, 3, 6, nvals, qbits);
+	find_thread_start(qf, keys, 4, 6, nvals, qbits);
+	find_thread_start(qf, keys, 5, 6, nvals, qbits);
 	int blocksPerRegion = 1;
 	int numRegions = qf->metadata->nblocks;
 
@@ -1958,7 +1962,7 @@ void qf_insert_gpu(QF* qf, uint64_t* keys, uint64_t value, uint64_t count, uint6
 	uint64_t block_size = ceil(qf->metadata->nslots / num_threads);
 	uint64_t block_offset = 0;
 	for (int tid = 0; tid < num_threads; tid++) {
-		
+		int t_start = find_thread_start(qf, keys, tid, num_threads, qbits);
 		blockend = tid * block_size + block_offset;
 		if (tid == 0) {
 			t_start = 0;
