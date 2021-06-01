2225d016e9c57b312f758a7167aa2d76e86f091d
Ericwimsatt <ericwimsatt@gmail.com>
2021-06-01 13:18:12 -0700

    more includes

--- src/gqf.cu
+++ src/gqf.cu
@@ -50,15 +50,7 @@
         gpuAssert((ans), __FILE__, __LINE__);                                            \
 
     }
-	inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
-	{
-		if (code != cudaSuccess)
-		{
-			fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-			if (abort)
-				exit(code);
-		}
-	}
+
 
 #ifdef DEBUG
 #define PRINT_DEBUG 1
