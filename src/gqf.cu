e1959ca41dbeacd5cfc93455c6d1d06ea6f16fcf
Ericwimsatt <ericwimsatt@gmail.com>
2021-06-19 12:04:42 -0700

    use temp to copy back to host

--- src/gqf.cu
+++ src/gqf.cu
@@ -2026,9 +2026,11 @@ __host__ void  qf_kernel(QF* qf, uint64_t* vals, uint64_t nvals, uint64_t nhashb
 	qfruntime runtime;
 	qfmetadata metadata;
 	qfblock blocks;
-	CUDA_CHECK(cudaMemcpy(&runtime, _qf->runtimedata, sizeof(qfruntime) , cudaMemcpyDeviceToHost));
-	CUDA_CHECK(cudaMemcpy(&metadata, _qf->metadata, sizeof(qfmetadata), cudaMemcpyDeviceToHost));
-	CUDA_CHECK(cudaMemcpy(&blocks, _qf->blocks, qf_get_total_size_in_bytes(qf), cudaMemcpyDeviceToHost));
+//copy back to host
+	CUDA_CHECK(cudaMemcpy(&temp_qf, _qf, sizeof(QF), cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(&runtime, temp_qf->runtimedata, sizeof(qfruntime) , cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(&metadata, temp_qf->metadata, sizeof(qfmetadata), cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(&blocks, temp_qf->blocks, qf_get_total_size_in_bytes(qf), cudaMemcpyDeviceToHost));
 	qf->runtimedata = runtime;
 	qf->metadata = metadata;
 	qf->blocks = blocks;
